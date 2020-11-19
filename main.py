3ed6ab8746e3adea43ed768c5793ec123e0840ff
Wesley Tan <wesleytws2018@gmail.com>
2020-11-20 04:32:38 +1030

    Add arithmetic operations

---
+++ main.py
@@ -0,0 +1,10 @@
+from arithmetic import add, subtract, multiply, divide, mod
+
+# Calculate 12 + 89
+print(add(12, 89))
+
+# Calculate 37 - 4
+print(subtract(37, 4))
+
+# Calculate 5 * 5 * 5
+print(multiply(multiply(5, 5), 5))
